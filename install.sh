9d1ec79594ceffe3bd5d43611a58c09c52cc573d
root <root@1673805.xorek.cloud>
2026-05-16 23:50:35 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,30 +3,46 @@
 # Установка зависимостей
 echo "Установка зависимостей..."
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip golang-go git curl
+sudo apt-get install -y python3 python3-pip python3-venv golang-go git curl
 
 # Клонирование репозитория
 echo "Клонирование репозитория..."
-git clone https://github.com/hhu67/cyberchat.git
-cd cyberchat
+if [ -d "cyberchat" ]; then
+    echo "Директория cyberchat уже существует. Обновляем репозиторий..."
+    cd cyberchat
+    git pull
+else
+    git clone https://github.com/hhu67/cyberchat.git
+    cd cyberchat
+fi
+
+# Создание и активация виртуального окружения для Python
+echo "Создание виртуального окружения для Python..."
+python3 -m venv venv
+source venv/bin/activate
 
 # Установка Python зависимостей
 echo "Установка Python зависимостей..."
-pip3 install -r client_py/requirements.txt --break-system-packages
+pip install --upgrade pip
+pip install -r client_py/requirements.txt
 
 # Компиляция Go кода
 echo "Компиляция Go кода..."
 cd backend_go
+if [ ! -f "go.mod" ]; then
+    go mod init cyberchat
+fi
 go build -o cyberchat
 cd ..
 
 # Копирование бинарника в /usr/local/bin
 echo "Копирование бинарника в /usr/local/bin..."
 sudo cp backend_go/cyberchat /usr/local/bin/cyberchat
+sudo chmod +x /usr/local/bin/cyberchat
 
 # Добавление команды в ~/.bashrc
 echo "Добавление команды в ~/.bashrc..."
-echo 'alias cyberchat="cyberchat"' >> ~/.bashrc
+echo 'alias cyberchat=\"cyberchat\"' >> ~/.bashrc
 source ~/.bashrc
 
 echo "Установка завершена! Теперь вы можете запускать проект командой 'cyberchat'."
